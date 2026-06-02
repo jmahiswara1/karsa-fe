00754da72b3800890d7be906474be0f0c3df4624
jmahiswara1 <gadangjatumahiswara@gmail.com>
2026-06-02 17:34:58 +0700

    Init project

---
+++ src/lib/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { useAuthStore } from '../stores/auth.store';
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().accessToken;
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
